f3fd0b38879265e3945f1daedd5fcb6fd3c42f98
Starfall Production <starfallproduction42@gmail.com>
2019-06-29 22:29:45 +0700

    Use composition to make CVMainList DOM more simple

--- src/components/CVMainList.js
+++ src/components/CVMainList.js
@@ -3,7 +3,7 @@ import {Grid, Typography, Box} from '@material-ui/core';
 
 function CVMainList(props){
     const Desc = ()=>{
-        if(props.desc != undefined || props.desc != ''){
+        if(props.desc !== undefined || props.desc !== ''){
             return(
                 <Grid item xs={12}>
                     <Box paddingLeft={2} textAlign="left">
@@ -22,26 +22,24 @@ function CVMainList(props){
     return (
         <React.Fragment>
             <Grid container item xs={6} md={8} direction="row">
-                <Grid item xs={12}>
-                    <Box fontWeight="fontWeightBold">
-                        <Typography variant="h5">
-                            {props.title}
-                        </Typography>
-                    </Box>
-                </Grid>
+                <Box component={Grid} item xs={12} fontWeight="fontWeightBold">
+                    <Typography variant="h5">
+                        {props.title}
+                    </Typography>
+                </Box>
                 <Grid item xs={12}>
                     <Typography variant="body1">
                         {props.subtitle}
                     </Typography>
                 </Grid>
             </Grid>
-            <Grid container item xs alignItems="center" justify="flex-end">
-                <Box fontStyle="italic" fontWeight="fontWeightLight">
-                    <Typography variant="body1">
-                        {props.time}    
-                    </Typography>
-                </Box>
-            </Grid>
+            <Box component={Grid} container item xs alignItems="center" 
+                justify="flex-end" fontStyle="italic" fontWeight="fontWeightLight"
+            >
+                <Typography variant="body1">
+                    {props.time}    
+                </Typography>
+            </Box>
             <Desc/>
         </React.Fragment>
     );
